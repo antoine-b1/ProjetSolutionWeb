a061cec457ffc50d38a3bf49fe7cb3f7782c6c22
man2lol <clement.salingue@ecoles-epsi.net>
2025-03-22 23:25:25 +0100

    ajout du css indexUser

--- User/indexUser.php
+++ User/indexUser.php
@@ -13,14 +13,14 @@ require '../config/database.php';
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Accueil </title>
-    <link href="../css/styles.css" rel="stylesheet"/>
+    <link href="../css/indexUser.css" rel="stylesheet"/>
     <link rel="icon" type="image/png"
           href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTS5VYolg4PlHUkQ7wMn4lTENI-rS9XfFDTOg&s">
 </head>
 <body>
 
 <header>
-    <img src="https://www.nuitdelinfo.com/inscription/uploads/ecoles/573/logos/logo.png"
+    <img src="../image/logoEpsiWis/logoEpsi.png"
          alt="Logo EPSI/WIS" class="logoepsi" width="85" height="80">
     <h1 class="green"> Accueil </h1>
     <p class="center">
